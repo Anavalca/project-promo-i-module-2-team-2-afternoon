f41e717f54af936268c35bf563707f970d5ed245
mablancoalvarez <mablancoalvarez@gmail.com>
2020-03-07 14:41:28 +0100

    add js palette

--- _src/assets/js/02-palette.js
+++ _src/assets/js/02-palette.js
@@ -1,1 +1,51 @@
-'hola 2'
+'use strict';
+// Elementos de la paleta
+const paletteOne = document.querySelector('#colorOne');
+
+const paletteTwo = document.querySelector('#colorTwo');
+
+const paletteThree = document.querySelector('#color3');
+
+// Elementos de la tarjeta
+
+const titleCard = document.querySelector('#cardid');
+const socialKit = document.querySelector ('.list');
+
+
+function previewOne (){
+
+    titleCard.classList.add('.colorPalette1');
+    titleCard.classList.remove('.colorPalette2');
+    titleCard.classList.remove('.colorPalette3');
+
+
+
+};
+
+function previewTwo (){
+    titleCard.classList.add('.colorPalette2');
+    titleCard.classList.remove('.colorPalette1');
+    titleCard.classList.remove('.colorPalette3');
+
+
+
+};
+
+
+function previewThree (){
+    titleCard.classList.add('.colorPalette3');
+    titleCard.classList.remove('.colorPalette1');
+    titleCard.classList.remove('.colorPalette2');
+
+};
+
+
+
+
+console.log('hola')
+
+
+paletteOne.addEventListener('change',previewOne);
+paletteTwo.addEventListener('change', previewTwo);
+paletteThree.addEventListener('change', previewThree);
+
