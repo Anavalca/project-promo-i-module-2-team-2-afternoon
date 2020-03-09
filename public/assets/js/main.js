6b0a9263ad2c91492a138107b00aec97df415199
mablancoalvarez <mablancoalvarez@gmail.com>
2020-03-09 20:29:44 +0100

    add icons

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -103,7 +103,44 @@ paletteThree.addEventListener('click', previewThree);
 
 
 
+'use strict'
 
+const emailInput = document.querySelector('#email-input');
+const phoneInput = document.querySelector('#phone-input');
+const linkedinInput = document.querySelector('#linkedin-input');
+const githubInput = document.querySelector('#github-input');
+
+const emailIcon = document.querySelector('#btn-email');
+const phoneIcon = document.querySelector('#btn-phone');
+const linkedinIcon = document.querySelector('#btn-linkedin');
+const githubIcon = document.querySelector('#btn-github');
+
+
+function appearIconEmail (){
+
+ emailIcon.classList.remove('hidden');
+ 
+}
+
+function appearIconPhone (){
+    phoneIcon.classList.remove('hidden');
+}
+
+function appearIconLinkedin (){
+    linkedinIcon.classList.remove('hidden');
+}
+
+function appearIconGithub (){
+    githubIcon.classList.remove('hidden');
+    console.log('hey')
+}
+
+
+
+emailInput.addEventListener('keyup',appearIconEmail);
+phoneInput.addEventListener('keyup',appearIconPhone);
+linkedinInput.addEventListener('keyup',appearIconLinkedin);
+githubInput.addEventListener('keyup',appearIconGithub);
 'use strict';
 
 console.log('>> Ready :)');
