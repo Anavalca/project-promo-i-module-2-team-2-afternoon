f123e88c95d2df8895fc3175b564c0724623fc14
AnaValca <avc92j@gmail.com>
2020-03-14 23:03:37 +0100

    add final formValidation

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -159,54 +159,49 @@ linkedinInput.addEventListener('keyup',appearIconLinkedin);
 githubInput.addEventListener('keyup',appearIconGithub);
 'use strict'
 
-// restauredColorInput(event) {
-//     event.curretTargetclassList.remove('errorBorder');
+const input1 = document.querySelector('#name-input');
+const input2 = document.querySelector('#job-input');
+const input3 = document.querySelector('#email-input');
 
-// }
 
-function validateFormInput(inputKey){
-    let input = document.querySelector(inputKey);
+function validateFormInput(input){
    
     if(input.value === ''){
         input.classList.add('errorBorder');
         input.nextElementSibling.classList.remove('hiddenInputMessage');
-        // input('keyup',restauredColorInput());
         return false;
     } else{
+        input.classList.remove('errorBorder');
+        input.nextElementSibling.classList.add('hiddenInputMessage');
         return true;
     }
 }
-
 
 function buttonActivation() {
 
     let button = document.querySelector("#buttonValidation");
     let errorMesagge = document.querySelector("#error-message");
 
-    let returnInput1 = validateFormInput('#name-input');
-    let returnInput2 = validateFormInput('#job-input');
-    let returnInput3 = validateFormInput('.search');
-    let returnInput4 = validateFormInput('#email-input');
+    let returnInput1 = validateFormInput(input1);
+    let returnInput2 = validateFormInput(input2);
+    let returnInput3 = validateFormInput(input3);
 
 
-    if (returnInput1 === false || returnInput2 === false 
-    || returnInput3 === false || returnInput4 === false) {
-
+    if (returnInput1 === false || returnInput2 === false || returnInput3 === false) {
         button.setAttribute('disabled','disabled');
         button.classList.add('buttonDisabled');
         errorMesagge.classList.remove('hiddenMessage');
-    }
-    else {
+    } else {
         button.classList.remove('buttonDisabled');
         errorMesagge.classList.add('hiddenMessage');
         button.removeAttribute("disabled");
-        
     }
 }
 
-
 topShare.addEventListener('click', buttonActivation);
-
+input1.addEventListener('keyup', function(){validateFormInput(this)});
+input2.addEventListener('keyup', function(){validateFormInput(this)});
+input3.addEventListener('keyup', function(){validateFormInput(this)});
 
 
 
