b8f4d12a30e954951437f2ce7234a4bf6dbd6edb
Lili Rascon <lilianarasconestrada@gmail.com>
2020-03-16 18:18:43 +0100

    finish

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -157,65 +157,6 @@ emailInput.addEventListener('keyup',appearIconEmail);
 phoneInput.addEventListener('keyup',appearIconPhone);
 linkedinInput.addEventListener('keyup',appearIconLinkedin);
 githubInput.addEventListener('keyup',appearIconGithub);
-
-'use strict';
-//call elements
-const nameInputElem = document.querySelector('#name-input');
-const professionInputElem = document.querySelector('#job-input');
-const imageInputElem = document.querySelector('#image-input');
-const addImageInputElem = document.querySelector('#addImage');
-const emailInputElem = document.querySelector('#email-input');
-const phoneInputElem = document.querySelector('#phone-input');
-const linkedinInputElem = document.querySelector('#linkedin-input');
-const githubInputElem = document.querySelector('#github-input');
-
-//save information in local storage
-const saveInfoLocalStorage = readsaveInfoLocalStorage()
-console.log(saveInfoLocalStorage)
-
-//function that responds to click
-function savesaveInfoLocalStorage(evt){
-  saveInfoLocalStorage[evt.currentTarget.name] = evt.currentTarget.value;
-  setsaveInfoLocalStorage(saveInfoLocalStorage);
-}
-
-//function that saves in local storage
-function setsaveInfoLocalStorage(userInfo){
-  localStorage.setItem('userInfo',JSON.stringify(userInfo));
-}
-
-//function that reads local storage
-function readsaveInfoLocalStorage(){
-  let saveInfoLocalStorage = JSON.parse(localStorage.getItem('userInfo'));
-  if(saveInfoLocalStorage !== null){
-    return saveInfoLocalStorage;
-  } else {
-    return saveInfoLocalStorage = {};
-  }
-}
-
-//function to fill inputs
-function fillFormfromLocal(saveInfoLocalStorage){
-  const inputArray = document.querySelectorAll('input');
- 
-  for(let inputName of inputArray){
-    if(saveInfoLocalStorage[inputName.name] !== undefined){
-      inputName.value = saveInfoLocalStorage[inputName.name]
-    } else {
-      inputName.value = '';
-    }
-  }
-}
-fillFormfromLocal(saveInfoLocalStorage);
-nameInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
-professionInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
-imageInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
-addImageInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
-emailInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
-phoneInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
-linkedinInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
-githubInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
-
 'use strict'
 
 const input1 = document.querySelector('#name-input');
@@ -308,6 +249,63 @@ function resetAll() {
 btnReset.addEventListener('click', resetAll);
 
 
+'use strict';
+//call elements
+const nameInputElem = document.querySelector('#name-input');
+const professionInputElem = document.querySelector('#job-input');
+const imageInputElem = document.querySelector('#image-input');
+const addImageInputElem = document.querySelector('#addImage');
+const emailInputElem = document.querySelector('#email-input');
+const phoneInputElem = document.querySelector('#phone-input');
+const linkedinInputElem = document.querySelector('#linkedin-input');
+const githubInputElem = document.querySelector('#github-input');
+
+//save information in local storage
+const saveInfoLocalStorage = readsaveInfoLocalStorage()
+console.log(saveInfoLocalStorage)
+
+//function that responds to click
+function savesaveInfoLocalStorage(evt){
+  saveInfoLocalStorage[evt.currentTarget.name] = evt.currentTarget.value;
+  setsaveInfoLocalStorage(saveInfoLocalStorage);
+}
+
+//function that saves in local storage
+function setsaveInfoLocalStorage(userInfo){
+  localStorage.setItem('userInfo',JSON.stringify(userInfo));
+}
+
+//function that reads local storage
+function readsaveInfoLocalStorage(){
+  let saveInfoLocalStorage = JSON.parse(localStorage.getItem('userInfo'));
+  if(saveInfoLocalStorage !== null){
+    return saveInfoLocalStorage;
+  } else {
+    return saveInfoLocalStorage = {};
+  }
+}
+
+//function to fill inputs
+function fillFormfromLocal(saveInfoLocalStorage){
+  const inputArray = document.querySelectorAll('input');
+ 
+  for(let inputName of inputArray){
+    if(saveInfoLocalStorage[inputName.name] !== undefined){
+      inputName.value = saveInfoLocalStorage[inputName.name]
+    } else {
+      inputName.value = '';
+    }
+  }
+}
+fillFormfromLocal(saveInfoLocalStorage);
+nameInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
+professionInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
+imageInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
+addImageInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
+emailInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
+phoneInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
+linkedinInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
+githubInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
 
 'use strict';
 
