a8c594e6f5a887238b4de90794bffe763a05c346
A CH <ach@MacBook-Pro-de-A.local>
2020-03-17 19:30:55 +0100

    Add image functionality

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -248,7 +248,6 @@ function resetAll() {
 
 btnReset.addEventListener('click', resetAll);
 
-
 'use strict';
 //call elements
 const nameInputElem = document.querySelector('#name-input');
@@ -307,6 +306,58 @@ phoneInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
 linkedinInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
 githubInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
 
+'use strict';
+
+const fr = new FileReader();
+const uploadBtn = document.querySelector('.js__profile-trigger');
+const fileField = document.querySelector('.js__profile-upload-btn');
+const profileImage = document.querySelector('.js__profile-image');
+const profilePreview = document.querySelector('.js__profile-preview');
+
+
+/**
+ * Recoge el archivo añadido al campo de tipo "file"
+ * y lo carga en nuestro objeto FileReader para que 
+ * lo convierta a algo con lo que podamos trabajar.
+ * Añade un listener al FR para que ejecute una función
+ * al tener los datos listos
+ * @param {evento} e 
+ */
+function getImage(e){
+  var myFile = e.currentTarget.files[0];
+  fr.addEventListener('load', writeImage);
+  fr.readAsDataURL(myFile);
+}
+
+
+/**
+ * Una vez tenemos los datos listos en el FR podemos
+ * trabajar con ellos ;)
+ */
+function writeImage() {
+  /* En la propiedad `result` de nuestro FR se almacena
+   * el resultado
+   */
+  profileImage.style.backgroundImage = `url(${fr.result})`;
+  profilePreview.style.backgroundImage = `url(${fr.result})`;
+}
+
+
+/**
+ * Genera un click automático en nuesto campo de tipo "file"
+ * que está oculto
+ */
+function fakeFileClick() {
+ fileField.click(); 
+}
+
+/**
+ * Añadimos los listeners necesarios:
+ * - al botón visible para generar el click automático
+ * - al campo oculto para cuando cambie su value
+ */
+uploadBtn.addEventListener('click', fakeFileClick);
+fileField.addEventListener('change', getImage);
 'use strict';
 
 console.log('>> Ready :)');
