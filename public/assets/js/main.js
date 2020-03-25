af26bff784f92f1bf5ef47faee301e560717d2e2
cortesana <a.chdez2020@gmail.com>
2020-03-25 01:05:11 +0100

    Edit and organise JS refresh

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -308,53 +308,53 @@ const profileImageInput = document.querySelector('.js__profile-image');
 //detectar el click de los elementos radio falta para quedarse con el value
 
 //save information in local storage
-const saveInfoLocalStorage = readsaveInfoLocalStorage();
-console.log(saveInfoLocalStorage);
+let infoToObject = infoObjectGenerator();
+console.log(infoToObject);
 
 //function that responds to click
-function savesaveInfoLocalStorage(evt){
-  saveInfoLocalStorage[evt.currentTarget.name] = evt.currentTarget.value; //Usa el name del input y recoge el valor introducido
+function infoIntoLocalStorage(evt){
+  infoToObject[evt.currentTarget.name] = evt.currentTarget.value; //Usa el name del input y recoge el valor introducido
   //ejecutamos la función readsaveInfoLocalStorage() sobre los valores recogidos
-  setsaveInfoLocalStorage(saveInfoLocalStorage); //ejecutamos la función setsaveInfoLocalStorage() sobre los valores recogidos
+  saveInfoLocalStorage(infoToObject); //ejecutamos la función setsaveInfoLocalStorage() sobre los valores recogidos
 }
 
 //function that saves in local storage
-function setsaveInfoLocalStorage(userInfo){
-  localStorage.setItem('userInfo', JSON.stringify(userInfo));
+function saveInfoLocalStorage(userInfo){
+  localStorage.setItem('userInfo',JSON.stringify(userInfo));
   // Guarda en la memoria local 'user info' que es la info del usuario formateada como .json
 }
 
 //function that reads local storage
-function readsaveInfoLocalStorage(){
-  let saveInfoLocalStorage = JSON.parse(localStorage.getItem('userInfo')); //reclara y asigna un nuevo valor, parseador de de la info de usuario en formato .json
-  if(saveInfoLocalStorage !== null){ // devuelve el objeto saveInfoLocalStorage con la info del usuario
-    return saveInfoLocalStorage;
+function infoObjectGenerator(){
+  let infoToObject = JSON.parse(localStorage.getItem('userInfo')); //reclara y asigna un nuevo valor, parseador de de la info de usuario en formato .json
+  if(infoToObject !== null){ // devuelve el objeto saveInfoLocalStorage con la info del usuario
+    return infoToObject;
   } else {
-    return saveInfoLocalStorage = {}; // devuelve saveInfoLocalStorage como objeto vacío
+    return infoToObject = {};
+   // devuelve saveInfoLocalStorage como objeto vacío
   }
 }
 
 //function to fill inputs
-function fillFormfromLocal(saveInfoLocalStorage){
+function fillFormfromLocal(object){
   const inputArray = document.querySelectorAll('input'); // Declara variable que selecciona el input
   for(let inputName of inputArray){ //Recorre los inputs
-    if(saveInfoLocalStorage[inputName.name] !== undefined){ // Comprueba si hay info en campos con atributo name (con valor name, profession, phone, email) del objecto saveInfoLocalStorage
-      inputName.value = saveInfoLocalStorage[inputName.name] // Si hay, guarda el valor en inputName
-    } else {
-      inputName.value = '';
+    if(object[inputName.name] !== undefined){ // Comprueba si hay info en campos con atributo name (con valor name, profession, phone, email) del objecto saveInfoLocalStorage
+      inputName.value = object[inputName.name]
+    }// Si hay, guarda el valor en inputName
     }
   }
-}
 
-fillFormfromLocal(saveInfoLocalStorage);
-nameInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
-professionInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
-addImageInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
-emailInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
-phoneInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
-linkedinInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
-githubInputElem.addEventListener('keyup',savesaveInfoLocalStorage);
-/* imageFile.addEventListener('input', /* savesInfoLocalStorage); */
+fillFormfromLocal(infoToObject);
+nameInputElem.addEventListener('keyup',infoIntoLocalStorage);
+professionInputElem.addEventListener('keyup',infoIntoLocalStorage);
+imageInputElem.addEventListener('keyup',infoIntoLocalStorage);
+addImageInputElem.addEventListener('keyup',infoIntoLocalStorage);
+emailInputElem.addEventListener('keyup',infoIntoLocalStorage);
+phoneInputElem.addEventListener('keyup',infoIntoLocalStorage);
+linkedinInputElem.addEventListener('keyup',infoIntoLocalStorage);
+githubInputElem.addEventListener('keyup',infoIntoLocalStorage);
+/* imageFile.addEventListener('input',); */
 'use strict';
 
 const fr = new FileReader();
