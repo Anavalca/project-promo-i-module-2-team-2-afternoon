9e06185580673b54b1ea45ad285488469454c57b
mablancoalvarez <mablancoalvarez@gmail.com>
2020-03-26 09:48:46 +0100

    local storage

--- _src/assets/js/09-refresh.js
+++ _src/assets/js/09-refresh.js
@@ -1,8 +1,6 @@
 'use strict';
 //call elements
-// const paletteOne = document.querySelector('#colorOne');
-// const paletteTwo = document.querySelector('#colorTwo');
-// const paletteThree = document.querySelector('#color3');
+
 const nameInputElem = document.querySelector('#name-input');
 const professionInputElem = document.querySelector('#job-input');
 /* const imageInputElem = document.querySelector('#img-selector'); */
@@ -10,125 +8,213 @@ const emailInputElem = document.querySelector('#email-input');
 const phoneInputElem = document.querySelector('#phone-input');
 const linkedinInputElem = document.querySelector('#linkedin-input');
 const githubInputElem = document.querySelector('#github-input');
-/* const paletteElems = document.querySelectorAll('.check'); */
 
-/* const infoToObject = {
-  /* "palette": 1,
-  "name": "María García",
-  "job": "Front-end developer",
-  "phone": "+34 666666666",
-  "email": "mariagar@example.com",
-  "linkedin": "mariagar",
-  "github": "mariagar",
+
+
+const objetcData = {
+"palette": " ",
+  "name": " ",
+  "job": " ",
+  "phone": " ",
+  "email": " ",
+  "linkedin": " ",
+  "github": " ",
   // "image": "undefined",
-}; */
+};
 
 //detectar el click de los elementos radio falta para quedarse con el value
 
-//save information in local storage
-let infoToObject = infoObjectGenerator();
-console.log(infoToObject);
 
-
-function checkInput (evt){
-
-  let dataCheck = evt.currentTarget.value ;
-  saveInfoLocalStorage(dataCheck);
-}
-
-
-
-
-
-// function retrievePalette() {
-//   let saveInLocalStorage = saveInfoLocalStorage(infoToObject);
-//   if(saveInLocalStorage === palette2.value) {
-//     palette2Value.checked = true;
-//   return palette2Value;
-//   }
-//   else if(saveInLocalStorage === palette3.value) {
-//     palette3Value.checked = true;
-//     return palette3Value;
-//   }
-//   else {
-//     palette1Value.checked = true;
-//     return palette1Value;
-    
-//   } 
-  
-// }
-
-// function storePalette(){
-//   if(palette2.checked){
-//     infoToObject.palette = palette2.value;
-//       console.log(infoToObject);
-//   } else if (palette3.checked){
-//       infoToObject.palette = palette3.value;
-//       console.log(infoToObject);
-//   }  else {
-//       infoToObject.palette = palette1.value;
-//       console.log(infoToObject);
-//   }
-//    saveInfoLocalStorage();
-// }
-
-//function that responds to click
-function infoIntoLocalStorage(evt){
-  infoToObject[evt.currentTarget.name] = evt.currentTarget.value; // MAL PARA IMAGEN //Usa el name del input y recoge el valor introducido
-  //ejecutamos la función infoObjectGenerator() sobre los valores recogidos
-  saveInfoLocalStorage(infoToObject); //ejecutamos la función saveInfoLocalStorage() sobre los valores recogidos
-}
+//  A partir de aqui es para checkbox 
 
 //function that saves in local storage
-function saveInfoLocalStorage(userInfo){
-  localStorage.setItem('userInfo',JSON.stringify(userInfo));
+function saveInfoLocalStorage(data){
+  localStorage.setItem('userInfo',JSON.stringify(data));
   // Guarda en la memoria local 'user info' que es la info del usuario formateada como .json
 }
 
-//function that reads local storage
-function infoObjectGenerator(){
-  let infoToObject = JSON.parse(localStorage.getItem('userInfo')); //reclara y asigna un nuevo valor, parseador de de la info de usuario en formato .json
-  if(infoToObject !== null){ // devuelve el objeto infoToObject con la info del usuario
-    return infoToObject;
-  } else {
-    return infoToObject = {};
-   // devuelve infoToObject como objeto vacío
+
+function getInfoLocalStorage(){
+  objetcData = JSON.parse(localStorage.getItem('userInfo'));
+  if(objetcData.palette !== undefined){
+      if(objetcData.palette === palette2.value){
+        previewTwo ();
+          paletteTwo.checked = true;
+          paletteOne.checked = false;
+          paletteThree.checked = false
+      } else if(objetcData.palette === palette3.value){
+        previewThree ();
+          paletteThree.checked = true;
+          paletteTwo.checked = false;
+          paletteOne.checked = false
+      } else{
+        previewOne ();
+          paletteThree.checked = false;
+          paletteTwo.checked = false;
+          paletteOne.checked = true;
+      }
   }
+
+  if (objetcData.name !== ''){
+    nameInputElem.value = objetcData.name;
+  }
+  if (objetcData.job !== ''){
+    professionInputElem.value =objetcData.job;
+  }
+  if (objetcData.email !== ''){
+    emailInputElem.value = objetcData.email;
+  }
+  if (objetcData.phone !== ''){
+    phoneInputElem.value = objetcData.phone;
+  }
+  if (objetcData.linkedin !== ''){
+    linkedinInputElem.value = objetcData.linkedin;
+  }
+  if (objetcData.github !== ''){
+    githubInputElem.value = objetcData.github;
+  }
+  saveInfoLocalStorage();
+  getInfoLocalStorage ();
 }
-// SELECCIONAR CON CLASE.
-//function to fill inputs
 
-
-
-
-
-
-function retrieveInfo(object){
-  const inputArray = document.querySelectorAll('.inputInfo'); // Declara variable que selecciona el input
-  for(let inputName of inputArray){ //Recorre los inputs
-    if(object[inputName.name] !== undefined){// Comprueba si hay info en campos con atributo name (con valor name, profession, phone, email) del objecto infoToObject
-      inputName.value = object[inputName.name];
-    }// Si hay, guarda el valor en inputName
-    }
+function setTheme(){
+  if(paletteTwo.checked){
+    previewTwo ();
+  } else if(paletteThree.checked){
+    previewThree ();
+  } else {
+    previewOne ();
   }
+  saveInfoLocalStorage();
+  getInfoLocalStorage ();
+}
+
+function checkInput(evt){
+  let dataCheck = evt.currentTarget.value ;
+
+  saveInfoLocalStorage(dataCheck);
+  getInfoLocalStorage (dataCheck);
+  setTheme()
+}
+
 function checkLocalStorage (){
   if (localStorage.getItem('userInfo')!== null){
-      // retrievePalette();
-      // retrieveInfo();
+    getInfoLocalStorage();
+    
   }
 }
 
-nameInputElem.addEventListener('keyup',infoIntoLocalStorage);
-professionInputElem.addEventListener('keyup',infoIntoLocalStorage);
-/* imageInputElem.addEventListener('input',imageIntoLocalStorage); */
-emailInputElem.addEventListener('keyup',infoIntoLocalStorage);
-phoneInputElem.addEventListener('keyup',infoIntoLocalStorage);
-linkedinInputElem.addEventListener('keyup',infoIntoLocalStorage);
-githubInputElem.addEventListener('keyup',infoIntoLocalStorage);
+function storeFullName(){
+  objetcData.name = nameInputElem.value;
+  saveInfoLocalStorage();   
+}
+function storeJob(){
+objetcData.job = professionInputElem.value;
+saveInfoLocalStorage();
+}
+function storeEmail(){
+objetcData.email = emailInputElem.value;
+saveInfoLocalStorage();
+}
+function storeTelf(){
+objetcData.phone = phoneInputElem.value;
+saveInfoLocalStorage();
+}
+function storeLinkedin(){
+objetcData.linkedin = linkedinInputElem.value;
+saveInfoLocalStorage();
+}
+function storeGithub(){
+objetcData.github = githubInputElem.value;
+saveInfoLocalStorage();
+}
 
-paletteOne.addEventListener('change',checkInput );
-paletteTwo.addEventListener('change',checkInput );
-paletteThree.addEventListener('change',checkInput );
 
 
-checkLocalStorage ();
+
+
+
+
+
+
+
+paletteOne.addEventListener('change',checkInput);
+paletteTwo.addEventListener('change',checkInput);
+paletteThree.addEventListener('change',checkInput);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //function that responds to click
+// function infoIntoLocalStorage(evt){
+//   infoToObject[evt.currentTarget.name] = evt.currentTarget.value; // MAL PARA IMAGEN //Usa el name del input y recoge el valor introducido
+//   //ejecutamos la función infoObjectGenerator() sobre los valores recogidos
+//   saveInfoLocalStorage(infoToObject); //ejecutamos la función saveInfoLocalStorage() sobre los valores recogidos
+// }
+
+// //function that saves in local storage
+// function saveInfoLocalStorage(userInfo){
+//   localStorage.setItem('userInfo',JSON.stringify(userInfo));
+//   // Guarda en la memoria local 'user info' que es la info del usuario formateada como .json
+// }
+
+// //function that reads local storage
+// function infoObjectGenerator(){
+//   let infoToObject = JSON.parse(localStorage.getItem('userInfo')); //reclara y asigna un nuevo valor, parseador de de la info de usuario en formato .json
+//   if(infoToObject !== null){ // devuelve el objeto infoToObject con la info del usuario
+//     return infoToObject;
+//   } else {
+//     return infoToObject = {};
+//    // devuelve infoToObject como objeto vacío
+//   }
+// }
+// // SELECCIONAR CON CLASE.
+// //function to fill inputs
+
+
+
+
+
+
+// function retrieveInfo(object){
+//   const inputArray = document.querySelectorAll('.inputInfo'); // Declara variable que selecciona el input
+//   for(let inputName of inputArray){ //Recorre los inputs
+//     if(object[inputName.name] !== undefined){// Comprueba si hay info en campos con atributo name (con valor name, profession, phone, email) del objecto infoToObject
+//       inputName.value = object[inputName.name];
+//     }// Si hay, guarda el valor en inputName
+//     }
+//   }
+// function checkLocalStorage (){
+//   if (localStorage.getItem('userInfo')!== null){
+//       // retrievePalette();
+//       // retrieveInfo();
+//   }
+// }
+
+// nameInputElem.addEventListener('keyup',infoIntoLocalStorage);
+// professionInputElem.addEventListener('keyup',infoIntoLocalStorage);
+// /* imageInputElem.addEventListener('input',imageIntoLocalStorage); */
+// emailInputElem.addEventListener('keyup',infoIntoLocalStorage);
+// phoneInputElem.addEventListener('keyup',infoIntoLocalStorage);
+// linkedinInputElem.addEventListener('keyup',infoIntoLocalStorage);
+// githubInputElem.addEventListener('keyup',infoIntoLocalStorage);
+
+
+
